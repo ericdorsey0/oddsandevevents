7f598895ff677c632d0ad347595219bac4c86ea4
eric <dorseyjr000@gmail.com>
2025-02-17 14:51:12 -0600

    .\index.js

--- index.js
+++ index.js
@@ -6,3 +6,26 @@ function addNumber(num) {
   numberBank.push(num);
   render();
 }
+
+function sortFirstNumber() {
+  if (numberBank.lenghth === 0) return;
+  const num = numberBank.shift();
+  if (num % 2 === 0) {
+    evenNumbers.push(num);
+  } else {
+    oddNumbers.push(num);
+  }
+  render();
+}
+
+function sortAllNumbers() {
+  numberBank.forEach((num) => {
+    if (num % 2 === 0) {
+      evenNumbers.push(num);
+    } else {
+      oddNumbers.push(num);
+    }
+  });
+  numberBank = [];
+  render();
+}
